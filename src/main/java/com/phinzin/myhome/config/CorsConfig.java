306ce424303e1add71761904eddcb0124e0a356b
phinzin <phinzin2@gmail.com>
2023-11-19 06:38:52 +0000

    cors config

---
+++ src/main/java/com/phinzin/myhome/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.phinzin.myhome.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
