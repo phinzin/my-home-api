3380adb7bc251a33be1e85318d0baaa5695cb073
phinzin <phinzin2@gmail.com>
2023-11-19 06:57:29 +0000

    CORS

--- src/main/java/com/phinzin/myhome/MyHomeApplication.java
+++ src/main/java/com/phinzin/myhome/MyHomeApplication.java
@@ -2,6 +2,9 @@ package com.phinzin.myhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
 
--- src/main/java/com/phinzin/myhome/controller/MyHomeController.java
+++ src/main/java/com/phinzin/myhome/controller/MyHomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +19,7 @@ import com.phinzin.myhome.service.MyHomeService;
 
 
 @RestController
+@CrossOrigin(origins = "*,https://nhacuatui.azurewebsites.net", maxAge = 3600)
 @RequestMapping("/api/my-home")
 public class MyHomeController {
     
